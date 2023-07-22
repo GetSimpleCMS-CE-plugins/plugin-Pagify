c769da308632df99d967193a031dbea374bab1b7
risingisland <risingisland@yahoo.com>
2023-07-22 11:01:27 +0100

    Add files via upload

---
+++ pagify/lang/es.php
@@ -0,0 +1,13 @@
+<?php
+$i18n = array(
+    'ELLIPSIS' => "..."
+  , 'FIRST' => "|&lt;&lt;"
+  , 'LAST' => "&gt;&gt;|"
+  , 'NEXT' => "&gt;"
+  , 'PREVIOUS' => "&lt;"
+  , 'TITLE_FIRST' => "Ir a la primera página"
+  , 'TITLE_LAST' => "Ir a la última página"
+  , 'TITLE_NEXT' => "Ir a la página siguiente"
+  , 'TITLE_PREVIOUS' => "Regresar a la pagina anterior"
+  , 'TO_PAGE' => "Ir a la página "
+);
